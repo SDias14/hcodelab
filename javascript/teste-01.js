36fe0164e8b512f752ba071c5297097458c8f8d1
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-07-25 16:40:29 -0300

    if

---
+++ javascript/teste-01.js
@@ -0,0 +1,16 @@
+//decisão if 
+
+/*
+
+let position  = 2
+
+if (position <=2) {
+    console.log('Acesso permitido')
+} else {
+    console.log('Acesso negado')
+}
+
+// exemplo classica da estrutura if. 
+
+*/
+
