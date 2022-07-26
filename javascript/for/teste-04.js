f0be9da507c5d36426717b17230d444d2dd4cbef
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-26 14:42:38 -0300

    for

---
+++ javascript/for/teste-04.js
@@ -0,0 +1,12 @@
+//FOR : COMPOSTO POR TRES FASES 
+
+// 1 - INICIALIZACAO
+// 2 - CONDICAO
+// 3 - INCREMENTO
+
+let number = 5;
+
+for (let i = 0; i <= 10 ; i++) {
+   console.log(`${number} x ${i} = ${number * i}`);
+}
+
