12f2cd4644b6c3163d850c7e69f47027253e8960
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-01 12:00:22 -0300

    push e pop

---
+++ javascript/arrays/pop e push/index.js
@@ -0,0 +1,24 @@
+let ranking = [9,0,6,4,8,7,3,2,1,5];
+
+ranking.sort().reverse();
+
+console.log(ranking );
+
+// pop
+
+
+let removed = ranking.pop();
+
+console.log(removed);
+
+// push
+
+ranking.push(removed);
+
+console.log(ranking);
+
+
+
+
+
+
