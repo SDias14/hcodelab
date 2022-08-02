89d6ef33fc5b8a6f4b58cf4748e5a739e14b0f2c
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-02 15:58:04 -0300

    Create index.js

---
+++ javascript/arrays/atribuição por desestruturação/index.js
@@ -0,0 +1,11 @@
+let car = ["BMW", "Volvo", "Mini", "Ford", "Fiat" ];
+
+
+
+let [samCar, John, tricia, tex, ronald] = car
+
+console.log(samCar)
+console.log(John)
+console.log(tricia)
+console.log(tex)
+console.log(ronald) // isso é um desestruturamento de array. 
