34012029477843965ba7da5d20eacd7262788921
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-03 10:51:13 -0300

    json stringfy

---
+++ javascript/JSON/stringfy/json.js
@@ -0,0 +1,8 @@
+let person = {
+    firstName : 'Samuel',
+    lastName : 'Dias',
+    favoriteLanguages: ['php', 'javascript', 'java']
+}
+console.log(person)
+console.log(JSON.stringify(person, null, 3)) // esse metodo json vai transformar o objeto pessoa em string, json nao suporta funcao. 
+
