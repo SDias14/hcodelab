6a52773dfa95b76d84cb160956448e9a1311e5d6
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-04 11:08:18 -0300

    Update spread.js

--- javascript/JSON/sprea-operator/spread.js
+++ javascript/JSON/sprea-operator/spread.js
@@ -2,11 +2,11 @@ let person = {
     name : "john",
     age : 30,
 
-}
+}// primeiro objeto que foi feito com json. tem os atributos nome e idade 
 
 let user = {
     email: 'samueld2014@gmail.com',
     password: 123456,
-}
+}// segundo objeto que foi feito tambem com json. tem os atributos email e senha.
 
-console.log({...person, ...user})// para fazer um spread posso usar os tres pontos 
+console.log({...person, ...user})// se usa a notação de objeto com as duas chaves dentro dos parenteses do console, indicando com tres pontos , fazendo com que a informaçãos seja espalhada. 
