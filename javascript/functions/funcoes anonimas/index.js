fdcd7ab099abe50b5dab220812718d15c12addb9
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-04 16:36:10 -0300

    funcoes anonimas

--- javascript/functions/funcoes anonimas/index.js
+++ javascript/functions/funcoes anonimas/index.js
@@ -1,11 +1,29 @@
-let calcAge = function(bornYear){
+let calcAge = function(bornYear, callback){
+
+    setTimeout(callback, 3000)
+
+
     return new Date().getFullYear() - bornYear
     }
 
-//console.log(calcAge(1996))// retornou a idade para fora do codigo 
-//console.log(calcAge(1975))// retornou a idade para fora do codigo 
+console.log(calcAge(1996, function(){
+    console.log('Voce é velho')
+})) // o segundo parametro que é uma funcao vai ser o parametro da primeira funcao , vai ser um callback que vai retornar uma mensagem depois de 3 segundos. 
 
-(function(){
-    console.log('Welcome hcode')
-})()//iief = imediately invoked function . É uma funcao que é chamada imediatamente ao rodar nosso codigo, no inicio dele e roda uma vez. 
+
+
+//let a = function () {
+ //   console.log('welcome hcode')
+//}
+
+//a()
+/*
+(function () {
+    console.log('hello hcode')
+})();
+
+*/
+
+
+//iief = imediately invoked function . É uma funcao que é chamada imediatamente ao rodar nosso codigo, no inicio dele e roda uma vez. 
 
