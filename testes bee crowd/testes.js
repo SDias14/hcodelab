568a3e8982dfd986414815cb3466c02ee403104d
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-04 17:37:56 -0300

    Create testes.js

---
+++ testes bee crowd/testes.js
@@ -0,0 +1,56 @@
+/* exercicio 1 
+
+let a = 5.0
+let b = 7.1
+
+let average = ((a*3.5 + b*7.5) / (3.5+7.5))
+
+console.log(average.toFixed(5))// desta maneira estou dizendo que quero 5 casas apos a virgula. 
+
+*/
+
+/*
+
+//exercicio 2 
+
+
+
+let a = 5.0
+let b = 6.0
+let c = 7.0
+
+let average = ((5.0*2 + 6.0*3 + 7.0*5) / (2+3+5))
+
+console.log(average.toFixed(1))
+
+*/
+
+/*
+
+//exercicio 3 
+
+let a = 5
+let b = 6 
+let c = 7
+let d = 8 
+
+let difference = (a*b) - (c*d)
+
+console.log(difference)
+
+*/
+
+//exercicio 4 
+
+/*
+
+const id = 25 
+let workedHours = 100
+let dollarPerHour = 5.50
+
+let salary = workedHours * dollarPerHour
+
+console.log(`The employee number ${id} earns US$ ${salary.toFixed(2)}`)
+
+*/
+
