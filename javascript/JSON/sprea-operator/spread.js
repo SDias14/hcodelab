115b0c40e92f01890fdee04f962541efabc12cf5
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-04 11:05:51 -0300

    spread para objetos

---
+++ javascript/JSON/sprea-operator/spread.js
@@ -0,0 +1,12 @@
+let person = {
+    name : "john",
+    age : 30,
+
+}
+
+let user = {
+    email: 'samueld2014@gmail.com',
+    password: 123456,
+}
+
+console.log({...person, ...user})// para fazer um spread posso usar os tres pontos 
