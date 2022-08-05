7d0cbd4881a773837ee0d7703d43e4ad51c5b73a
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-05 15:58:16 -0300

    manipulação do dom

---
+++ javascript/dom, objetos globais e apis da web/location/index.js
@@ -0,0 +1,3 @@
+//object location esta dentro da window, ou seja, conseguimos manipular o url da nossa pagina. 
+
+//location.href = 'index1.html' // redirecionou para outra pagina 
---
+++ javascript/dom, objetos globais e apis da web/manipulando formularios /index.js
@@ -0,0 +1,13 @@
+function getData (event) {
+
+    event.preventDefault() // retorna o formulario e previne de retornar na url 
+
+
+    let form = document.querySelector('form')
+
+    let data = {}
+   
+
+
+}
+
