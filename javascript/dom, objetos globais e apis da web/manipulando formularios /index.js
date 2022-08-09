f081c7e2cd497ce12a95bee6374b6844f23a63e9
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-09 07:29:35 -0300

    Update index.js

--- javascript/dom, objetos globais e apis da web/manipulando formularios /index.js
+++ javascript/dom, objetos globais e apis da web/manipulando formularios /index.js
@@ -1,13 +1,31 @@
-function getData (event) {
+function getData (evento) {
 
-    event.preventDefault() // retorna o formulario e previne de retornar na url 
+    evento.preventDefault() // retorna o formulario e previne de retornar na url 
 
 
     let form = document.querySelector('form')
 
-    let data = {}
+/*
+let data = {};
    
+[...form.querySelectorAll('input')].forEach((tag) => {
+    
+    data[tag.name] = tag.value
+})
 
+console.log(data)
+
+*/
+
+let form2 = new FormData(form)
+
+console.log(form2.get("name"))
+console.log(form2.get("pass"))
 
 }
+
+
+
+
+
 
