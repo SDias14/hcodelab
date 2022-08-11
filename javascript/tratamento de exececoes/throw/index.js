b85df565344215442b388d372aaa2d92a00fea7e
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-08-11 15:12:45 -0300

    Update index.js

--- javascript/tratamento de exececoes/throw/index.js
+++ javascript/tratamento de exececoes/throw/index.js
@@ -1,8 +1,28 @@
+function numberException () {
+  this.message = 'is not a number'
+  this.name = 'numberException'
+}
+
+function zeroException () {
+  this.message = 'not divisible by zero'
+  this.name = 'zeroException'
+}
+
+
+
+
 function divide(a, b) {
+
+if(typeof a !=='number' || typeof b !=='number'){
+  throw new numberException ().message;
+} else if (b === 0){
+  throw new zeroException ().message
+} 
+
   return a / b;
 }
 
 
 console.log(divide(4, 2)) // 2
-console.log(divide(100, '25px')) 
-console.log(divide(100, 0)) // Infinity 
+console.log(divide(100, 0)) 
+console.log(divide(100, "50")) // throws an error
