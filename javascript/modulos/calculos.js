c90eb2456ecdaa23006ae614a0ca6685af7a70b1
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-08-12 09:41:53 -0300

    node module

---
+++ javascript/modulos/calculos.js
@@ -0,0 +1,28 @@
+function somar (a, b) {
+  return a + b;
+}
+
+function subtrair (a, b) {
+  return a - b;
+}
+
+function multiplicar (a, b) {
+  return a * b;
+}
+
+function dividir (a, b) {
+  return a / b;
+}
+
+
+function regra3 (a, b, c) {
+  return (a * c) / b;
+}
+
+module.exports = { 
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    regra3,
+};
---
+++ javascript/modulos/module-node.js
@@ -0,0 +1,8 @@
+const {somar, subtrair, multiplicar, dividir, regra3} = require('./calculos.js'); // importou do arquivo calculos.js
+
+console.log(somar(1,2));
+console.log(subtrair(4,2));
+console.log(multiplicar(4,2));
+console.log(dividir(4,2));
+console.log(regra3(1000,500,200));
+
